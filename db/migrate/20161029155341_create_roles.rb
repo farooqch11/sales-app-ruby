4ca9b03b55a71e6384eff9a3e77f87cb34669493
Faizan Ahmad <faizu_ali@yahoo.com>
2016-10-30 15:53:40 +0500

    add Role in migation

--- db/migrate/20161029155341_create_roles.rb
+++ db/migrate/20161029155341_create_roles.rb
@@ -1,4 +1,14 @@
 class CreateRoles < ActiveRecord::Migration
+  def migrate(direction)
+    super
+    # Create a default Roles
+    Role.create!([{id: 1 , name: "General Manager"},
+                  {id: 2 , name: "Store Manager/ Supervisor"} ,
+                  {id: 3 , name: "Cashier"} ,
+                  {id: 4 , name: "Customer Service Manager"},
+                  {id: 5 , name: "Inventory Manager"},
+                  {id: 6 , name: "Warehouse manager"}])
+  end
   def change
     create_table :roles do |t|
       t.string :name
