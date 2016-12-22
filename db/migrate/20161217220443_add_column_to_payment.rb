9085c4ac33cc896a8c90190d3eff82edb099c7c2
Faizan Ahmad <faizu_ali@yahoo.com>
2016-12-22 23:54:42 +0500

    migration file

--- db/migrate/20161217220443_add_column_to_payment.rb
+++ db/migrate/20161217220443_add_column_to_payment.rb
@@ -1,6 +1,7 @@
 class AddColumnToPayment < ActiveRecord::Migration
   def change
     add_column :payments, :payment_term, :integer
-    change_column :payments, :payment_type, :integer
+    remove_column :payments, :payment_type
+    add_column :payments, :payment_type, :integer
   end
 end
