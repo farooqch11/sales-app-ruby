56e88f1b9bc65527c0400f58916dc118c20702aa
Faizan Ahmad <faizu_ali@yahoo.com>
2017-02-06 02:36:08 +0500

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170113231937) do
+ActiveRecord::Schema.define(version: 20170125210315) do
 
   create_table "addresses", force: :cascade do |t|
     t.string   "address_1"
@@ -102,13 +102,29 @@ ActiveRecord::Schema.define(version: 20170113231937) do
     t.text     "purpose"
     t.datetime "paid_time"
     t.string   "expense_type"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
     t.float    "low_stock_alert"
     t.float    "amount"
+    t.integer  "payment_method",  default: 0
+    t.string   "ref_no"
+    t.string   "slug"
   end
 
   add_index "expenses", ["company_id"], name: "index_expenses_on_company_id"
+
+  create_table "friendly_id_slugs", force: :cascade do |t|
+    t.string   "slug",                      null: false
+    t.integer  "sluggable_id",              null: false
+    t.string   "sluggable_type", limit: 50
+    t.string   "scope"
+    t.datetime "created_at"
+  end
+
+  add_index "friendly_id_slugs", ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
+  add_index "friendly_id_slugs", ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
+  add_index "friendly_id_slugs", ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id"
+  add_index "friendly_id_slugs", ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type"
 
   create_table "item_categories", force: :cascade do |t|
     t.integer  "company_id"
@@ -116,9 +132,11 @@ ActiveRecord::Schema.define(version: 20170113231937) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "slug"
   end
 
   add_index "item_categories", ["company_id"], name: "index_item_categories_on_company_id"
+  add_index "item_categories", ["slug"], name: "index_item_categories_on_slug", unique: true
 
   create_table "items", force: :cascade do |t|
     t.string   "sku"
@@ -156,6 +174,8 @@ ActiveRecord::Schema.define(version: 20170113231937) do
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
     t.boolean  "published",  default: true
+    t.string   "slug"
+    t.integer  "status",     default: 0
   end
 
   create_table "locations_users", id: false, force: :cascade do |t|
@@ -230,6 +250,33 @@ ActiveRecord::Schema.define(version: 20170113231937) do
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
   end
+
+  create_table "taggings", force: :cascade do |t|
+    t.integer  "tag_id"
+    t.integer  "taggable_id"
+    t.string   "taggable_type"
+    t.integer  "tagger_id"
+    t.string   "tagger_type"
+    t.string   "context",       limit: 128
+    t.datetime "created_at"
+  end
+
+  add_index "taggings", ["context"], name: "index_taggings_on_context"
+  add_index "taggings", ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], name: "taggings_idx", unique: true
+  add_index "taggings", ["tag_id"], name: "index_taggings_on_tag_id"
+  add_index "taggings", ["taggable_id", "taggable_type", "context"], name: "index_taggings_on_taggable_id_and_taggable_type_and_context"
+  add_index "taggings", ["taggable_id", "taggable_type", "tagger_id", "context"], name: "taggings_idy"
+  add_index "taggings", ["taggable_id"], name: "index_taggings_on_taggable_id"
+  add_index "taggings", ["taggable_type"], name: "index_taggings_on_taggable_type"
+  add_index "taggings", ["tagger_id", "tagger_type"], name: "index_taggings_on_tagger_id_and_tagger_type"
+  add_index "taggings", ["tagger_id"], name: "index_taggings_on_tagger_id"
+
+  create_table "tags", force: :cascade do |t|
+    t.string  "name"
+    t.integer "taggings_count", default: 0
+  end
+
+  add_index "tags", ["name"], name: "index_tags_on_name", unique: true
 
   create_table "users", force: :cascade do |t|
     t.integer  "company_id"
